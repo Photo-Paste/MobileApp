54a3f7dffe67c1806d197c7df36e554e3551faac
NoJamesNo <jamesnoh123@gmail.com>
2024-01-27 18:26:15 -0500

    sajdaskj

--- photopaster/App.js
+++ photopaster/App.js
@@ -1,10 +1,28 @@
+import React from 'react';
+import { StyleSheet, Text, View, Button } from 'react-native';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import auth from '@react-native-firebase/auth';
+import { GoogleSignin } from '@react-native-google-signin/google-signin';
+
+GoogleSignin.configure({
+  webClientId: 'YOUR_WEB_CLIENT_ID', // Get this from the Google Cloud Console
+});
+
+const signInWithGoogleAsync = async () => {
+  try {
+    const { idToken } = await GoogleSignin.signIn();
+    const googleCredential = auth.GoogleAuthProvider.credential(idToken);
+    return auth().signInWithCredential(googleCredential);
+  } catch (error) {
+    console.error(error);
+  }
+};
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
+      <Button title="Sign in with Google" onPress={() => signInWithGoogleAsync()} />
       <StatusBar style="auto" />
     </View>
   );
