5a1342e9c97a9fcd8e3a9878d5e0220f33eb956c
gollanai <gollanai@msu.edu>
2024-01-28 09:53:46 -0500

    ocr

--- src/app/ocr.tsx
+++ src/app/ocr.tsx
@@ -37,6 +37,8 @@ export const useOCR = () => {
             }
           ]
         };
+
+        console.log(requestPayload)
   
         try {
           const response = await fetch(visionApiUrl, {
